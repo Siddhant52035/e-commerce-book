7073677711130f35978f17cd5c614696cffca865
Siddhant Thakur <ps.siddhant.52035@gmail.com>
2025-05-08 14:42:30 +0545

    sid commit final

--- backend/models/noteModel.js
+++ backend/models/noteModel.js
@@ -2,14 +2,13 @@ import mongoose, { Schema } from "mongoose";
 
 const noteSchema = new mongoose.Schema(
   {
-    userId: { type: Schema.Types.ObjectId, ref: "Users", required:true },
+    userId: { type: Schema.Types.ObjectId, ref: "Users", required: true },
     name: { type: String, required: true },
-    pdf: { type: String, required: true },
-    desc: { type: String, required: true }, 
-    comments: [{type:Schema.Types.ObjectId, ref:"Comments"}],
-    
+    pdf: { type: Buffer, required: true }, // Now stores binary PDF data
+    desc: { type: String, required: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
   },
-  { timestamps: true } 
+  { timestamps: true }
 );
 
 const Notes = mongoose.model("Notes", noteSchema);
